5a501558f6e00e3f359023fbc1c5b76d194edaa5
rmondin <richardmondin@hotmail.com>
2020-03-30 23:43:59 -0300

    Validação de rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const ongController = require('./controllers/ongController');
 const incidentController = require('./controllers/incidentController');
-const profileController  = require('./controllers/profileController');
-const sessionController  = require('./controllers/sessionController');
+const profileController = require('./controllers/profileController');
+const sessionController = require('./controllers/sessionController');
 
 console.log(ongController);
 console.log(incidentController);
@@ -11,14 +12,35 @@ const routes = express.Router();
 
 
 routes.get('/ongs', ongController.getAll);
-routes.post('/ongs', ongController.create);
+
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        nome: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        cidade: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), ongController.create);
 routes.delete('/ongs/:id', ongController.remove);
 
-routes.get('/profile', profileController.getAll);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), profileController.getAll);
 
-routes.get('/incidents', incidentController.getAll);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    }),
+}), incidentController.getAll);
 routes.post('/incidents', incidentController.create);
-routes.delete('/incidents/:id', incidentController.remove);
+routes.delete('/incidents/:id',celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    }),
+}), incidentController.remove);
 
 
 routes.post('/sessions', sessionController.create);
